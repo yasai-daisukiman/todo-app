223291cbfe566118f9e272b1009dad34b971a50f
arai <yamadaacaunto@gmail.com>
2024-01-16 02:19:46 +0900

    add: タスク欄をスクロールエリア化&レイアウト調整

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -6,13 +6,16 @@ const TopPage = () => {
   return (
     <>
       <Layout>
-        <main className=' flex flex-col items-center justify-center'>
-          <h1 className=' '>ToDoリスト</h1>
-          <div className='w-full max-w-xl'>
-            <InputTask />
-            <ToDoList />
+        <div className='flex flex-row justify-center'>
+          <div className='flex flex-row gap-8 w-5/6'>
+            <div className='w-1/2 '>
+              <InputTask />
+            </div>
+            <div className='w-1/2 h-96'>
+              <ToDoList />
+            </div>
           </div>
-        </main>
+        </div>
       </Layout>
     </>
   );
