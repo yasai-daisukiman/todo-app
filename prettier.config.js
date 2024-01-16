7c35120b5625806d6f8542111d2647e85b54d3cb
arai <yamadaacaunto@gmail.com>
2024-01-16 16:08:53 +0900

    add: prettier-plugin-tailwindcssのインポート

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  semi: true,
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+};
