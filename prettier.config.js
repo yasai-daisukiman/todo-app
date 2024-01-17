0e08aa6cdaf89af86d575dde0e845ba910a8970a
arai <yamadaacaunto@gmail.com>
2024-01-17 18:33:05 +0900

    remove: 不要ファイル削除

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-  semi: true,
-  singleQuote: true,
-  printWidth: 90,
-  tabWidth: 2,
-  trailingComma: 'all',
-  jsxSingleQuote: true,
-};
