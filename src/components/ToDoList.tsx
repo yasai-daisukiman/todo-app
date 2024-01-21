791c66ce1b7dacdc2f2fb057e91e8a7f80b67838
arai <yamadaacaunto@gmail.com>
2024-01-21 17:02:48 +0900

    add: タスクを日付ごとにまとめて表示

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -7,10 +8,13 @@ import { db } from '@/firebase/firebase';
 import { ViewToDo } from './ViewToDo';
 import { TaskProps } from '@/types/TodoList';
 import { ScrollArea } from './ui/scroll-area';
+import dayjs from 'dayjs';
+import { Label } from './ui/label';
 
 export const ToDoList = () => {
-  const [todos, setTodos] = useState<TaskProps[]>();
+  const [groupedData, setGroupedData] = useState({});
 
+  //firestoreからリアルタイムでデータ取得
   useEffect(() => {
     const getData = () => {
       const collectionRef = collection(db, 'todos');
@@ -21,10 +25,21 @@ export const ToDoList = () => {
             ({
               ...doc.data(),
               id: doc.id,
-              date: doc.data().date.toDate(),
+              date: dayjs(doc.data().date.toDate()).format('YYYY/MM/DD'),
             }) as unknown as TaskProps,
         );
-        setTodos(data);
+
+        // フォーマットしたデータをソート
+        const sortedData = data.sort(
+          (a, b) => dayjs(a.date).unix() - dayjs(b.date).unix(),
+        );
+
+        const groupedData = sortedData.reduce((prev, current) => {
+          const date = current.date;
+          prev[date] = prev[date] ? [...prev[date], current] : [current];
+          return prev;
+        }, {});
+        setGroupedData(groupedData);
       });
     };
     getData();
@@ -33,9 +48,14 @@ export const ToDoList = () => {
   return (
     <>
       <ScrollArea className='h-full rounded-md border border-primary'>
-        <ul className='px-4 py-2'>
-          {todos?.map((todo) => <ViewToDo key={todo.id} todo={todo} />)}
-        </ul>
+        {Object.keys(groupedData).map((date) => (
+          <div key={date} className='pt-2'>
+            <Label className='pl-4'>{date}</Label>
+            {groupedData[date].map((todo: TaskProps) => (
+              <ViewToDo key={todo.id} todo={todo} />
+            ))}
+          </div>
+        ))}
       </ScrollArea>
     </>
   );
--- src/components/ViewToDo.tsx
+++ src/components/ViewToDo.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 'use client';
-
 import { db } from '@/firebase/firebase';
 import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { Button } from './ui/button';
@@ -30,7 +29,6 @@ import {
 import { CiEdit } from 'react-icons/ci';
 import { FaCheck } from 'react-icons/fa';
 import { MdDeleteOutline } from 'react-icons/md';
-import dayjs from 'dayjs';
 
 type ToDoProps = {
   todo: TaskProps;
@@ -45,7 +43,6 @@ const formSchema = z.object({
 export const ViewToDo = ({ todo }: ToDoProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const [newTitle, setNewTitle] = useState(todo.title);
-  const date = dayjs(todo.date).format('YYYY/MM/DD');
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -78,74 +75,75 @@ export const ViewToDo = ({ todo }: ToDoProps) => {
   };
 
   return (
-    <li key={todo.id} className='flex w-full flex-row gap-4'>
-      <div className='collapsible my-2 w-4/5 font-bold'>
-        {isEditing ? (
-          <form onSubmit={form.handleSubmit(handleSave)}>
-            <Input
-              className='w-full rounded-md border border-primary px-2'
-              value={newTitle}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setNewTitle(e.target.value);
-              }}
-            />
-          </form>
-        ) : (
-          <Accordion
-            type='multiple'
-            className='flex w-full flex-col rounded-md border px-2'
-          >
-            <AccordionItem value='item-1'>
-              <AccordionTrigger>
-                <div>{todo.title}</div>
-              </AccordionTrigger>
-              <AccordionContent className=' font-normal'>
-                <p>{todo.desc}</p>
-                <p>{date}</p>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        )}
-      </div>
-      <div className='flex w-1/5 flex-row items-center justify-center gap-2'>
-        {isEditing ? (
-          <Button
-            size='icon'
-            className='bg-green-800 hover:bg-green-800'
-            onClick={handleSave}
-          >
-            <FaCheck />
-          </Button>
-        ) : (
-          <Button
-            size='icon'
-            className='bg-green-600 hover:bg-green-600/70'
-            onClick={handleEdit}
-          >
-            <CiEdit className='size-6' />
-          </Button>
-        )}
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button size='icon' className='bg-red-600 hover:bg-red-600/70'>
-              <MdDeleteOutline className='size-6' />
+    <ul className='px-4'>
+      <li key={todo.id} className='flex w-full flex-row gap-4'>
+        <div className='collapsible my-2 w-4/5 font-bold'>
+          {isEditing ? (
+            <form onSubmit={form.handleSubmit(handleSave)}>
+              <Input
+                className='w-full rounded-md border border-primary px-2'
+                value={newTitle}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setNewTitle(e.target.value);
+                }}
+              />
+            </form>
+          ) : (
+            <Accordion
+              type='multiple'
+              className='flex w-full flex-col rounded-md border px-2'
+            >
+              <AccordionItem value='item-1'>
+                <AccordionTrigger>
+                  <div>{todo.title}</div>
+                </AccordionTrigger>
+                <AccordionContent className=' font-normal'>
+                  <p>{todo.desc}</p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          )}
+        </div>
+        <div className='flex w-1/5 flex-row items-center justify-center gap-2'>
+          {isEditing ? (
+            <Button
+              size='icon'
+              className='bg-green-800 hover:bg-green-800'
+              onClick={handleSave}
+            >
+              <FaCheck />
             </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
-    </li>
+          ) : (
+            <Button
+              size='icon'
+              className='bg-green-600 hover:bg-green-600/70'
+              onClick={handleEdit}
+            >
+              <CiEdit className='size-6' />
+            </Button>
+          )}
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button size='icon' className='bg-red-600 hover:bg-red-600/70'>
+                <MdDeleteOutline className='size-6' />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete your account
+                  and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      </li>
+    </ul>
   );
 };
