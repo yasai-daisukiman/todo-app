f38a42e5e98ae4c52f5bc97ed3ea4199fa1291c4
arai <yamadaacaunto@gmail.com>
2024-01-29 23:10:10 +0900

    update: トップページの内容変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,10 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <Layout>
-      <main className='flex flex-col p-12 items-center'>
+      <main className='flex flex-col items-center p-12'>
         <div>
-          <h2 className='font-medium text-4xl text-center'>Welcome to MyPage!</h2>
-          <p className='py-4'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio tenetur, in,
-            temporibus commodi doloribus, nobis obcaecati adipisci aspernatur laborum rem
-            accusantium deleniti pariatur. Repudiandae esse temporibus optio possimus
-            voluptatibus tenetur, ipsum debitis ratione quas quo consectetur ullam dolorem
-            nobis eaque necessitatibus eos vitae vel sunt tempore nostrum quam culpa
-            pariatur?
-          </p>
+          <h2 className='text-4xl font-medium'>Welcome to Todo-App😆</h2>
+          <p className='max-w-md py-4 text-center'>This is a simple todo-app.</p>
         </div>
         <div className='py-4'>
           <Link href='/login'>
